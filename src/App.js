0161b24969f5a535ac64b39fafd646deb80ab49e
poojithapalladi <122811760+poojithapalladi@users.noreply.github.com>
2024-09-05 18:46:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom' 
+ import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { EventProvider } from './context/EventContext'; 
+import CalendarView from './Components/CalendarView'; 
+import EventDetails from './Components/EventDetails'; 
 
-const App=()=>{
+const App = () => {
     return (
-        <BrowserRouter>
-         <Routes>
-            <Route  path='/' element={<CalendarView/>}/>
-            <Route path='/event/:id'  element={<EventDetails/>}/>
-            </Routes>
-         </BrowserRouter>
-
-     )
+        <EventProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<CalendarView />} />
+                    <Route path='/event/:id' element={<EventDetails />} />
+                </Routes>
+            </BrowserRouter>
+        </EventProvider>
+    );
 }
-export default App
+
+export default App;
