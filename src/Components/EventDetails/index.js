c080f241a5b6e4614ad906106c58816d7d5906cc
poojithapalladi <122811760+poojithapalladi@users.noreply.github.com>
2024-09-05 18:47:43 +0530

    Update index.js

--- src/Components/EventDetails/index.js
+++ src/Components/EventDetails/index.js
@@ -1,21 +1,25 @@
-import React from'react' 
-import { useParams } from 'react-router-dom' 
-import './EventDetails.css' 
+ import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import './EventDetails.css';
+import { EventContext } from '../../context/EventContext'; 
 
-const EventDetails=({events})=>{
-    const {id}=useParams();
-    const event=events.find((event)=>event.id===Number(id));
-    
-    if (!event){
-        return <p>Event not found</p>
+const EventDetails = () => {
+    const { id } = useParams();
+    const { events } = useContext(EventContext); 
+
+    const event = events.find((event) => event.id === Number(id));
+
+    if (!event) {
+        return <p>Event not found</p>;
     }
+
     return (
         <div className='event-details'>
             <h1>{event.title}</h1>
-            <p>{event.date.toLocalDateString()}</p>
+            <p>{new Date(event.date).toLocaleDateString()}</p> 
             <p>{event.category}</p>
         </div>
-    )
-
+    );
 }
-export default EventDetails
+
+export default EventDetails;
